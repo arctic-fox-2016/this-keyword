75adae8ea4bf8f3f7fe7c242a170d6eae04ec008
Ari Adiprana & Andrew Tandiawan <pair+ariadiprana.atandiawan@hacktiv8.com>
2016-09-05 11:49:26 +0700

    release 0 dan 1

--- this.js
+++ this.js
@@ -1,11 +1,32 @@
 "use strict"
 
 // GLOBAL CONTEXT
+let meja = {
+  kaki : 4,
+  tambahkaki: function() {
+    this.kaki++
+  }
+}
+
+console.log(meja.kaki)
+meja['tambahkaki']()
+console.log(meja.kaki)
 
 
+// RELEASE 0
+// Syntax:
+// node -> this [enter]
+// versi node yang terinstall adalah
+// version: 'v6.3.1',
 
 
 // CLASS CONTEXT
 
 
 // RELEASE 1
+
+//The value of this is dependent upon two things:
+// 1. The type of code being executed (i.e., global, function, or eval)
+// 2. the caller of that code.
+
+//this refers to the global object in all global code.
