1e037f92006042cd9a795c69e9758d76c5468414
Imam & Toni <pair+imamnugraha23.tonychen09@hacktiv8.com>
2016-09-05 12:00:46 +0700

    imam & tony

--- this.js
+++ this.js
@@ -1,11 +1,25 @@
 "use strict"
 
 // GLOBAL CONTEXT
-
+/*
+ketik 'node' di terminal untuk masuk ke node REPL
+ketik this.process.version untuk mengecek version dari node
+*/
 
 
 
 // CLASS CONTEXT
+class rumah{
+  constructor(harga, jenis)
+  {
+      this.harga = harga
+      this.jenis = jenis
+  }
+}
 
+var accessRumah = new rumah(222,"small")
+console.log(accessRumah.harga)
 
 // RELEASE 1
+//this adalah variabel global
+//this jika digunakan oleh objek dapat digunakan untuk memanggil properties sebuah objek
