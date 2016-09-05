b0a66c2bc5aff00ad6c5e37e39a9834861624eca
Fadhli Ramadhani Arislan & Peter Raswono <pair+fadhlirams.praswono@hacktiv8.com>
2016-09-05 12:09:04 +0700

    Fadhli Peter

--- this.js
+++ this.js
@@ -1,11 +1,19 @@
 "use strict"
 
-// GLOBAL CONTEXT
-
-
-
+// GLOBAL CONTEXTß
+// this.process.version
+// Version node JS adalah: 'v6.4.0'
 
 // CLASS CONTEXT
+class Example {
+  constructor(contoh) {
+    this.contoh = contoh
 
+  }
+}
+let property = new Example(4)
+property.contoh = "test"
+console.log(property.contoh)
 
 // RELEASE 1
+// 'This' adalah sebuah variable yang digunakan untuk memanggil object atau function dari dalam object/function tersebut.
