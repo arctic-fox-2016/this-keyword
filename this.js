b99d9f3165bd9f32f4b1ee51e77ff3f5eddabc6d
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-05 20:30:01 +0700

    Ahmad Ahmadi & Ryan Andhaka

--- this.js
+++ this.js
@@ -6,6 +6,29 @@
 
 
 // CLASS CONTEXT
+class Animal {
+
+
+  constructor (components) {
+    this.name = components ['name']
+    this.age = components ['age']
+    this.foot = components ['foot']
+  }
+
+
+    get name(){
+      this.name = name
+      return this.name
+    }
+
+    get age(){
+      this.age = age
+      return this.age
+    }
+
+
+    let hewan = new Animal ({name: 'kucing', age: '3'})
+    console.log(hewan.name());
 
 
 // RELEASE 1
